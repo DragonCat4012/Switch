b9afdf9103c2e94e1ef148bacfa6bc6566ec52de
Kiara <frida.shivaa@gmail.com>
2024-04-25 17:13:41 +0200

    basic main menu wuuuuu

--- MainMenu.gd
+++ MainMenu.gd
@@ -1,5 +1,7 @@
 extends MarginContainer
 
+const first_scene = preload("res://game.tscn")
+
 @onready var selector_one = $CenterContainer/VBoxContainer/CenterContainer2/HBoxContainer2/VBoxContainer/Selector
 @onready var selector_two = $CenterContainer/VBoxContainer/CenterContainer2/HBoxContainer2/VBoxContainer/Selector2
 @onready var selector_three = $CenterContainer/VBoxContainer/CenterContainer2/HBoxContainer2/VBoxContainer/Selector3
@@ -10,7 +12,28 @@ func _ready():
 	set_current_selection(0)
 	
 func _process(delta):
-	return
+	if Input.is_action_just_pressed("ui_down"):
+		currentSelection += 1
+	elif Input.is_action_just_pressed("ui_up"):
+		currentSelection -= 1
+
+	if currentSelection < 0:
+		currentSelection = 2
+	if currentSelection > 2:
+		currentSelection = 0
+		
+	set_current_selection(currentSelection)
+	if Input.is_action_just_pressed("ui_accept"):
+		handle_selection(currentSelection)
+	
+func handle_selection(_current_selection):
+	if _current_selection == 0: #start option
+		get_parent().add_child(first_scene.instantiate())
+		queue_free()
+	elif _current_selection == 1: # Options
+		print("Add Options uwu")
+	elif _current_selection == 2: # Exit
+		get_tree().quit()
 	
 func set_current_selection(_current_selection):
 	selector_one.text = ""
