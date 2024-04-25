85ca7f645cd106dcef27aae4fe8d1d0b9d86a214
kiara <sophie_natalie.zieschang@mailbox.tu-dresden.de>
2024-04-25 11:22:17 +0200

    hm

--- playerObject.gd
+++ playerObject.gd
@@ -4,15 +4,19 @@ class_name Player extends Node2D
 @export var tablePosition: int = 0 #not set
 @export var switches = []
 
+# init
+func init(tablePosition=0,switches=[]): #pseudo-constructor
+	#some action like follows
+	tablePosition=tablePosition
+	switches=switches
+	
 # Override virtual methods
 func _ready():
 	initSwitchStates()
 
-func setTablePosition(pos: int):
-	tablePosition = pos
 # lamps
 func initSwitchStates():
-	return
+	return 
 	
 func toggle(lamp:int):
 	return 
