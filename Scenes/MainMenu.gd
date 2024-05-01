7afdb623740b33fe28ce7ef7a2b073a505465f36
Kiara <68660034+DragonCat4012@users.noreply.github.com>
2024-05-01 15:39:32 +0200

    fix always selecting first lol

--- Scenes/MainMenu.gd
+++ Scenes/MainMenu.gd
@@ -5,31 +5,33 @@ extends MarginContainer
 @onready var selector_one = $CenterContainer/VBoxContainer/CenterContainer2/HBoxContainer2/VBoxContainer/Selector
 @onready var selector_two = $CenterContainer/VBoxContainer/CenterContainer2/HBoxContainer2/VBoxContainer/Selector2
 @onready var selector_three = $CenterContainer/VBoxContainer/CenterContainer2/HBoxContainer2/VBoxContainer/Selector3
-@onready var labelTutorial = $CenterContainer/VBoxContainer/CenterContainer2/HBoxContainer2/VBoxContainer2/Label
-@onready var labelStart = $CenterContainer/VBoxContainer/CenterContainer2/HBoxContainer2/VBoxContainer2/Label2
-@onready var labelExit = $CenterContainer/VBoxContainer/CenterContainer2/HBoxContainer2/VBoxContainer2/Label3
+@onready var labelTutorial = $CenterContainer/VBoxContainer/CenterContainer2/HBoxContainer2/VBoxContainer2/TutorialLabel
+@onready var labelStart = $CenterContainer/VBoxContainer/CenterContainer2/HBoxContainer2/VBoxContainer2/LabelStart
+@onready var labelExit = $CenterContainer/VBoxContainer/CenterContainer2/HBoxContainer2/VBoxContainer2/LabelExit
 
 var currentSelection = 0
 var lastSelection = 0
 
 func _ready():
-	set_current_selection(0)
+	set_current_selection()
 	
 func _input(event): # Handle Touch Inut
-	var newSelection = 0
+	var newSelection = -1
 	if event is InputEventMouseButton:
 		if labelTutorial.get_global_rect().has_point(get_global_mouse_position()):
 			newSelection = 1
-		if labelStart.get_global_rect().has_point(get_global_mouse_position()):
+		elif labelStart.get_global_rect().has_point(get_global_mouse_position()):
 			newSelection = 0
-		if labelTutorial.get_global_rect().has_point(get_global_mouse_position()):
-			labelExit = 2
-			
-	if currentSelection == newSelection:
-		handle_selection(currentSelection)
-	else:
-		currentSelection = newSelection
-		set_current_selection(newSelection)
+		elif labelExit.get_global_rect().has_point(get_global_mouse_position()):
+			newSelection = 2
+		else:
+			newSelection = 0
+		print(newSelection)
+		if currentSelection == newSelection:
+			handle_selection(currentSelection)
+		else:
+			currentSelection = newSelection
+			set_current_selection()
 	
 func _process(delta):
 	if Input.is_action_just_pressed("ui_down"):
@@ -38,13 +40,8 @@ func _process(delta):
 		currentSelection -= 1
 	elif Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
-
-	if currentSelection < 0:
-		currentSelection = 2
-	if currentSelection > 2:
-		currentSelection = 0
 		
-	set_current_selection(currentSelection)
+	set_current_selection()
 	if Input.is_action_just_pressed("ui_accept"):
 		handle_selection(currentSelection)
 	
@@ -56,7 +53,12 @@ func handle_selection(_current_selection):
 	elif _current_selection == 2: # Exit
 		get_tree().quit()
 	
-func set_current_selection(_current_selection):
+func set_current_selection():
+	if currentSelection < 0:
+		currentSelection = 2
+	if currentSelection > 2:
+		currentSelection = 0
+		
 	if currentSelection != lastSelection:
 		if not $"../AudioStreamPlayer".playing:
 			$"../AudioStreamPlayer".play()
@@ -65,11 +67,11 @@ func set_current_selection(_current_selection):
 	selector_two.text = ""
 	selector_three.text = ""
 		
-	if _current_selection == 0 :
+	if currentSelection == 0 :
 		selector_one.text = ">"
-	elif _current_selection == 1:
+	elif currentSelection == 1:
 		selector_two.text = ">"
-	elif _current_selection == 2:
+	elif currentSelection == 2:
 		selector_three.text=">"
 		
-	lastSelection = _current_selection
+	lastSelection = currentSelection
