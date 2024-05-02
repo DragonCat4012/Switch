19fd50b14b36c795505c687dd0a7d4858740c9c7
kiara <sophie_natalie.zieschang@mailbox.tu-dresden.de>
2024-05-02 13:46:06 +0200

    save score on back

--- game.gd
+++ game.gd
@@ -103,6 +103,7 @@ func _process(delta):
 func _input(event): # Handle Touch Inut
 	var globalRect = backButton.get_global_rect()
 	if globalRect.has_point(get_global_mouse_position()) and event is InputEventScreenTouch:
+		jsonHandler.saveScore(score)
 		get_tree().change_scene_to_file("res://Scenes/menu.tscn") 
 
 func _init_game():
