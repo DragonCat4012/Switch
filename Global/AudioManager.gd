5cdc7b087f2f95fccd5fe88c2e13944c56d7dd2e
Kiara <frida.shivaa@gmail.com>
2024-05-13 20:24:33 +0200

    fix soundmanger not playing in loop

--- Global/AudioManager.gd
+++ Global/AudioManager.gd
@@ -2,23 +2,21 @@ extends AudioStreamPlayer
 
 const main_backgorund_music = preload("res://Sounds/daylight.mp3")
 const zap_sound = preload("res://Sounds/ping.mp3")
-
+var should_play_background_music = true
 
 func _play_music(music: AudioStream, volume = -30.0):
-	if stream == music:
-		return
+	should_play_background_music = true
 	
 	stream = music
 	volume_db = volume
-	if !playing:
-		play()
-		connect("finished", Callable(self,"_on_loop_sound").bind(self))
+	play()
+	connect("finished", Callable(self,"_on_loop_sound").bind(self))
 	
 func playZap():
 	_play_single_sound_effect(zap_sound)
 	
 func play_music_background():
-	_play_music(main_backgorund_music)
+	_play_music(zap_sound)
 
 func _play_single_sound_effect(stream: AudioStream, volume = -40.0):
 	var player = AudioStreamPlayer.new()
@@ -32,4 +30,4 @@ func _play_single_sound_effect(stream: AudioStream, volume = -40.0):
 	player.queue_free()
 	
 func _on_loop_sound(player):
-	player.stream_paused = false
+	_play_music(main_backgorund_music)
