bd5f6a89329a2eebb003aecf09f9d45746b555fc
kiara <sophie_natalie.zieschang@mailbox.tu-dresden.de>
2024-05-26 18:30:27 +0200

    fix OptionsToggle

--- Scenes/OptionsScene.gd
+++ Scenes/OptionsScene.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var endianToggle = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/HBoxContainer
+@onready var endianToggle = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/HBoxContainer/TextureRect
 var isEndianSwitchingEnabled = false
 
 var onTexture = load("res://Sprites/Toggle/on.PNG")
@@ -17,13 +17,13 @@ func _ready():
 	
 func _input(event):
 	var currentClickTime = Time.get_ticks_msec()
-	if event is InputEventScreenTouch and endianToggle.get_global_rect().has_point(get_global_mouse_position()) and currentClickTime > lastClick + 0.8*1000:
+	if backButton.get_global_rect().has_point(get_global_mouse_position()) and event is InputEventScreenTouch:
+		get_tree().change_scene_to_file("res://Scenes/menu.tscn") 
+	if endianToggle.get_global_rect().has_point(get_global_mouse_position()) and event is InputEventScreenTouch and currentClickTime > lastClick + 0.5*1000:
 		lastClick = Time.get_ticks_msec()
 		isEndianSwitchingEnabled = !isEndianSwitchingEnabled
 		GameManager.jsonHandler.updatEndian(isEndianSwitchingEnabled)
 		updateEndianSwitch()
-	if backButton.get_global_rect().has_point(get_global_mouse_position()) and event is InputEventScreenTouch:
-		get_tree().change_scene_to_file("res://Scenes/menu.tscn") 
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
@@ -32,8 +32,7 @@ func _process(delta):
 
 # UI
 func updateEndianSwitch():
-	var toggle = $CenterContainer/VBoxContainer/CenterContainer2/VBoxContainer/HBoxContainer/SwitchEndian
 	if isEndianSwitchingEnabled:
-		toggle.texture = onTexture
+		endianToggle.texture = onTexture
 	else:
-		toggle.texture = offTexture
+		endianToggle.texture = offTexture
